5da4076870c59df3bec6e89ccaf7dc880e49042b
EnverJ <Enver1987@gmail.com>
2019-07-23 15:05:45 -0600

    This is second command

---
+++ src/HnadsonPractice/MyGit.java
@@ -0,0 +1,14 @@
+package HnadsonPractice;
+
+public class MyGit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int  ar[]={1,2,3,4,5};
+for (int s:ar){
+	System.out.println(s);
+	
+}
+	}
+
+}
